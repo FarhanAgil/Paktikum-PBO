3cc619300cf71cfae1e1226c467666abbe17b600
FarhanAgil <60720596+FarhanAgil@users.noreply.github.com>
2020-09-09 15:18:29 +0700

    Add files via upload

---
+++ Pertemuan 2/Pertemuan 2 (teori)/code uml java/Model/Persegi.java
@@ -0,0 +1,52 @@
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class Persegi {
+
+    /**
+     * Default constructor
+     */
+    public Persegi() {
+    }
+
+    /**
+     * 
+     */
+    public int sisi;
+
+    /**
+     * @return
+     */
+    public void InfoSisi() {
+        // TODO implement here
+        return null;
+    }
+
+    /**
+     * @return
+     */
+    public int hitungLuas() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @return
+     */
+    public int hitungKeliling() {
+        // TODO implement here
+        return 0;
+    }
+
+    /**
+     * @return
+     */
+    public void tampilPersegi() {
+        // TODO implement here
+        return null;
+    }
+
+}
---
+++ Pertemuan 2/Pertemuan 2 (teori)/laporan.md
@@ -0,0 +1,30 @@
+~ Latihan 1
+
+1)	Identifikasi, ada berapa class yang akan dibuat dari sistem diatas?
+Jawab :
+Ada 2 class yaitu mahasiswa dan dosen
+
+2) 	Implementasikan class dalam bahasa pemrograman java.
+Jawab :
+1.
+<img src = "dosen1.png">
+2.
+<img src = "mahasiswa1.png">
+
+~ Latihan 2
+
+	Tambahkan secara lengkap attribute pada setiap class yang sudah Anda identifikasi pada latihan sebelumnya (Latihan 1)?
+
+1.<img src = "mahasiswa.png">
+2.<img src = "dosen.png">
+
+~latihan 3 
+
+Hasil generate  : 
+<img src = "persegi.png">
+
+<img src = "persegi1.png">
+
+~screenshot staruml: 
+
+<img src = "staruml1.jpg">
